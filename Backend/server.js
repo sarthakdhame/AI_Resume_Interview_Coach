43f03fda2812a117da9f02d98cc8004c364f6892
sarthakdhame07 <sarthakdhame3673@gmail.com>
2026-03-30 10:17:27 +0530

    backend ready for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,13 @@ const connectToDB = require("./src/config/database")
 
 connectToDB()
 
+const cors = require("cors");
+
+app.use(cors({
+    origin: "*"
+}));
+
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
 })
